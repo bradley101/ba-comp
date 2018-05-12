d947c1c5ceb4825f1429e538cb25a6368b1d0afa
bradley101 <shantanu.banerjee.vt@gmail.com>
2018-05-12 14:56:50 +0530

    Create parse syntax for '-' operator and numbers with length more than 1

--- bacomp.py
+++ bacomp.py
@@ -2,7 +2,7 @@ class TokenTypes:
     def __init__(self):
         pass
 
-    DIGIT = 'DIGIT'
+    NUMBER = 'NUMBER'
     SYMBOL = 'SYMBOL'
     EOT = 'EOT'
 
@@ -20,22 +20,24 @@ class Interpreter:
 
     def eval(self):
         a = self.get_next_token()
-        self.check_type(a, TokenTypes.DIGIT)
+        self.check_type(a, TokenTypes.NUMBER)
         o = self.get_next_token()
         self.check_type(o, TokenTypes.SYMBOL)
         b = self.get_next_token()
-        self.check_type(b, TokenTypes.DIGIT)
+        self.check_type(b, TokenTypes.NUMBER)
         e = self.get_next_token()
         self.check_type(e, TokenTypes.EOT)
 
         if o.val == '+':
             return a.val + b.val
+        elif o.val == '-':
+            return a.val - b.val
         else:
             raise Exception('Invalid arithmetic operator')
 
     def check_type(self, current_token, required_type):
         if current_token.type == required_type:
-            self.pos += 1
+            self.pos += len(str(current_token.val))
             return
         raise Exception('Invalid token at {}'.format(self.pos + 1))
 
@@ -44,10 +46,19 @@ class Interpreter:
             return Token(TokenTypes.EOT)
 
         elif 48 <= ord(self.text[self.pos]) <= 57:
-            return Token(TokenTypes.DIGIT, ord(self.text[self.pos]) - 48)
+            p1 = self.pos + 1
+            while p1 < len(self.text) and 48 <= ord(self.text[p1]) <= 57:
+                p1 += 1
+            return Token(TokenTypes.NUMBER, int(self.text[self.pos:p1]))
 
         elif self.text[self.pos] == '+':
             return Token(TokenTypes.SYMBOL, self.text[self.pos])
+
+        elif self.text[self.pos] == '-':
+            return Token(TokenTypes.SYMBOL, self.text[self.pos])
+
+        else:
+            raise Exception('Invalid token')
 
 
 def main():
